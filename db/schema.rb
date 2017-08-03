33cb33c03be9726d0981b51a2dd68dfce5159d91
Stan <solve153@gmail.com>
2017-08-03 18:58:09 +0800

    generate attachment migration to resume

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170803104444) do
+ActiveRecord::Schema.define(version: 20170803105722) do
 
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20170803104444) do
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "attachment"
   end
 
   create_table "users", force: :cascade do |t|
