9d41bc671716e24a47440de3b0fd7d9d11432580
Stan <solve153@gmail.com>
2017-08-03 22:54:20 +0800

    add seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+create_account = User.create([email: 'admin@test.com', password: '12345678', password_confirmation: '12345678', is_admin: 'true'])
+puts "Admin account created."
+
+create_jos = for i in 1..10 do
+  Job.create!([title: "Job no.#{i}", description: "第 #{i} 個Public工作", wage_upper_bound: rand(50..99)*100, wage_lower_bound: rand(10..49)*100, is_hidden: "false"])
+end
+puts "10 Public jobs created."
+
+create_jos = for i in 1..10 do
+  Job.create!([title: "Job no.#{i+10}", description: "第 #{i+10} 個Hidden工作", wage_upper_bound: rand(50..99)*100, wage_lower_bound: rand(10..49)*100,is_hidden: "true"])
+end
+puts "10 Hidden jobs created."
