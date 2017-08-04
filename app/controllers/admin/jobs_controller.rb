5120274c0eef9c966b612f9d5fb37641b28d790c
Stan <solve153@gmail.com>
2017-08-04 10:41:03 +0800

    fix undefined admin issue

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -8,7 +8,7 @@ class Admin::JobsController < ApplicationController
   end
 
   def index
-    @jobs = Job.all.paginate(:page => params[:page], :per_page => 5)
+    @jobs = Job.all.recent.paginate(:page => params[:page], :per_page => 5)
   end
 
   def new
